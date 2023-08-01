16ef83ee906dba42e78b76299637cb3e968b6ac4
saad <saadneosoft@gmail.com>
2023-08-01 13:39:46 +0530

    cell reordarable

--- searchbar/ViewController.swift
+++ searchbar/ViewController.swift
@@ -11,11 +11,12 @@ class ViewController: UIViewController {
     
     var maindata = ["saad", "aditi" ,"chintan","saad", "aditi" ,"chintan","saad", "aditi" ,"chintan","saad", "aditi" ,"chintan"]
     var searcheddata : [String] = []
-
+    
     @IBOutlet weak var searchbar: UISearchBar!
     @IBOutlet weak var tableview: UITableView!
     
     override func viewDidLoad() {
+        self.tableview.isEditing = true
         searcheddata = maindata
         tableview.delegate = self
         tableview.dataSource = self
@@ -24,7 +25,7 @@ class ViewController: UIViewController {
     }
     
 }
-extension ViewController : UITableViewDelegate , UITableViewDataSource , UISearchBarDelegate {
+extension ViewController :UITableViewDelegate , UITableViewDataSource , UISearchBarDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return searcheddata.count
     }
@@ -47,8 +48,42 @@ extension ViewController : UITableViewDelegate , UITableViewDataSource , UISearc
         searchbar.endEditing(true)
         tableview.reloadData()
     }
-    heigh
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
+
+    func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let item = maindata[sourceIndexPath.row]
+        maindata.remove(at: sourceIndexPath.row)
+        maindata.insert(item, at: destinationIndexPath.row)
+    }
+    
     
     
 }
 
+//    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+//        return .none
+//    }
+//
+//    func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+//        return false
+//    }
+//
+//    func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+//        let movedobj = self.searcheddata[sourceIndexPath.row]
+//        searcheddata.remove(at: sourceIndexPath.row)
+////        searcheddata.insert(movedobj, at: proposedDestinationIndexPath.row)
+//        searcheddata.insert(movedobj, at: proposedDestinationIndexPath.row)
+//
+//    }
+                        // we can only use above function when we use tableview controller
