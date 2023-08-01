e5c4ad89e0a8c157c0caec78893daa545d137030
saad <saadneosoft@gmail.com>
2023-08-01 10:50:57 +0530

    basic search bar

--- searchbar/ViewController.swift
+++ searchbar/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var maindata = ["saad", "aditi" ,"chintan","saad", "aditi" ,"chintan","saad", "aditi" ,"chintan","saad", "aditi" ,"chintan"]
+    var searcheddata : [String] = []
 
+    @IBOutlet weak var searchbar: UISearchBar!
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
+        searcheddata = maindata
+        tableview.delegate = self
+        tableview.dataSource = self
+        searchbar.delegate = self
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+}
+extension ViewController : UITableViewDelegate , UITableViewDataSource , UISearchBarDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searcheddata.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        cell.userlabel.text = searcheddata[indexPath.row]
+        return cell
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searcheddata = searchText.isEmpty ? maindata : maindata.filter{
+            $0.lowercased() == searchText.lowercased()
+        }
+        tableview.reloadData()
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchbar.text = ""
+        searcheddata = maindata
+        searchbar.endEditing(true)
+        tableview.reloadData()
+    }
+    heigh
+    
+    
 }
 
